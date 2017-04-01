ee833361104b2012e6f20e9b7694983321aa6dc3
shreyasi123 <chaudharyshre@gmail.com>
2017-04-01 14:25:51 +0530

    Update second.js

--- p5/second.js
+++ p5/second.js
@@ -1,8 +1,20 @@
 function setup() { 
   createCanvas(400, 400);
-} 
+} var x=35
+  var y= 150
 
 function draw() { 
   background(220);
-	rect(10,10,50,15)
+	arc(x,150,50,50,PI/4,-PI/4,PIE)
+  if(x>50) {}
+	else{ellipse(50,150,5,5)}
+  if(x>70) {}
+  else{ellipse(70,150,5,5)}
+	if(x>90) {}
+	else{ellipse(90,150,5,5)}
+	if(x>110) {}
+	else{ellipse(110,150,5,5)}
+	if(x>130) {}
+	else{ellipse(130,150,5,5)}
+	x=x+1
 }
